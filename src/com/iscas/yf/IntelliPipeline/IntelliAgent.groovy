5c70e1134c9e4d1eb6848bc48409a995a6aa410f
YangFeng <250970437@qq.com>
2018-07-09 23:06:41 +0800

    Update IntelliAgent.groovy

--- src/com/iscas/yf/IntelliPipeline/IntelliAgent.groovy
+++ src/com/iscas/yf/IntelliPipeline/IntelliAgent.groovy
@@ -255,7 +255,7 @@ public class IntelliAgent{
     @NonCPS
     def executePostRequest(body){
 
-        def post = new URL(http://39.104.105.27:8888/IntelliPipeline/build_data/upload").openConnection();
+        def post = new URL(http://localhost:8888/IntelliPipeline/build_data/upload").openConnection();
 
         post.setRequestMethod("POST")
         post.setDoOutput(true)
